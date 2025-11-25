84bf8c2eccd9147220d352781e404f94d0a0d88a
vek03 <vektromboni@gmail.com>
2025-11-25 16:03:55 -0300

    :tada: First Commit

---
+++ src/main/java/com/vekrest/vekconsumer/vekconsumer/VekconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.vekrest.vekconsumer.vekconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VekconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VekconsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vekrest/vekconsumer/vekconsumer/VekconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vekrest.vekconsumer.vekconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VekconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
