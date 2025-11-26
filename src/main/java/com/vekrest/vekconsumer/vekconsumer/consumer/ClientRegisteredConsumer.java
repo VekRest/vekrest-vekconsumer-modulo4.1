a23eadc679b5cc213446fbc1b711c5998275c1cf
vek03 <vektromboni@gmail.com>
2025-11-26 13:03:43 -0300

    feat: kafka consumer

---
+++ src/main/java/com/vekrest/vekconsumer/vekconsumer/consumer/ClientRegisteredConsumer.java
@@ -0,0 +1,32 @@
+package com.vekrest.vekconsumer.vekconsumer.consumer;
+
+import com.vekrest.vekconsumer.vekconsumer.entities.Client;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.TopicSuffixingStrategy;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClientRegisteredConsumer {
+    @RetryableTopic(
+            autoCreateTopics = "false",
+            backoff = @Backoff(
+                    delay = 15000,
+                    multiplier = 2.0,
+                    maxDelay = 54000
+            ),
+            topicSuffixingStrategy = TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE
+    )
+    @KafkaListener(
+            topics = "${spring.kafka.topic.coxinha}",
+            groupId = "${spring.kafka.consumer.group-id}",
+            containerFactory = "kafkaListenerFactory"
+    )
+    public void listener(@Payload ConsumerRecord<String, Client> consumerRecord) {
+        System.out.println("KEY: " + consumerRecord.key());
+        System.out.println("VALUE: " + consumerRecord.value());
+    }
+}
